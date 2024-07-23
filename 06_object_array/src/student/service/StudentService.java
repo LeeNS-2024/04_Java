741592c7cd6882c5e34ebcb0e12d9535781ca0e0
LeeNS-2024 <dlsktj1@gmail.com>
2024-07-23 17:47:34 +0900

    20240723_수업 내용 커밋(object.src.student)

--- 06_object_array/src/student/service/StudentService.java
+++ 06_object_array/src/student/service/StudentService.java
@@ -49,34 +49,36 @@ public class StudentService {
 			std.setJs(   random.nextInt(101) );
 			std.setJava( random.nextInt(101) );
 		}
-		
 	}
 
+	
 	/**
 	 * 학생 객체 배열 students 중 비어있는 인덱스에
 	 * 전달 받은 학생 객체(std) 추가
-	 * @param std : 입력받은 값을 저장한 학생 객체 주소
-	 * @return true : 추가 성공 / false : 추가 실패
+	 * @param std : 입력 받은 값을 저장한 학생 객체 주소
+	 * @return true : 추가 성공  /  false : 추가 실패
 	 * - 실패 조건 : students 배열에 꽉 차있는데 추가 학생이 전달된 경우
 	 */
-	
 	public boolean addStudent(StudentDTO std) {
 		
-		// 0번 인덱스부터
+		// 0번 인덱스 부터 
 		// 순서대로 비어있는(null) 요소를 찾아
 		// 전달 받은 std를 대입
 		
-		for(int i = 0; i < students.length; i++) {
-			
+		
+		
+		for(int i = 0 ; i < students.length ; i++) {
 			if(students[i] == null) { // i 번째 요소가 비어있는 경우
 				students[i] = std;
 				return true;
 			}
+			
 		}
 		
 		// 비어있는(null)인 요소가 없을 경우 false 반환
 		return false;
 	}
+	
 	
 	/**
 	 * students 객체 배열에 저장된 주소를 얻어가게 하는 메서드
@@ -86,14 +88,15 @@ public class StudentService {
 		return students;
 	}
 
+
 	/**
 	 * 학생 1명 정보 조회 (index 검색)
 	 * @param index
-	 * @return 조회된 학생 객체 주소 || null
+	 * @return 조회된 학생 객체 주소  ||  null
 	 */
 	public StudentDTO selectIndex(int index) {
 		
-		// 전달받은 index 범위가 students 의 인덱스 범위 밖인 경우
+		// 전달 받은 index 범위가 students의 인덱스 범위 밖인 경우
 		if(index < 0 || index >= students.length) {
 			return null;
 		}
@@ -101,14 +104,15 @@ public class StudentService {
 		return students[index];
 	}
 	
+	
 	/**
-	 * 전달받은 이름과 이름이 일치하는 학생 객체를 반환 
+	 * 전달 받은 이름과 이름이 일치하는 학생 객체를 반환
 	 * @param targetName
 	 * @return null 또는 일치하는 학생 객체(주소 값)
 	 */
 	public StudentDTO selectName(String targetName) {
 		
-		// 향상된 for 문
+		// 향상된 for문
 		for(StudentDTO std : students) {
 			
 			// 저장된 학생이 없을 경우
@@ -117,15 +121,123 @@ public class StudentService {
 			
 			// == : 기본 자료형 비교
 			// A.equals(B) : 참조형(객체) 비교
-			if(std.getName().equals(targetName)) {
+			
+			if( std.getName().equals(targetName) ) {
 				return std;
 			}
 		}
 		
 		return null;
 	}
+
+
+	/**
+	 * 전달 받은 index가 students 범위 내인지
+	 *  + 정상 범위라면 해당 index가 학생 객체를 참조하는지 확인
+	 * @param index
+	 * @return 1 또는 2 또는 3(정상)
+	 */
+	public int checkIndex(int index) {
+		// 입력 받은 index가 정상인지 판별
+		// 1 == 범위 초과
+		// 2 == 학생 없음
+		// 3 == 정상
+		
+		if(index < 0 || index >= students.length) return 1;
+		if(students[index] == null) return 2;
+		return 3;
+	}
 	
+	/**
+	 * 전잘받은 index 번째 학생의 점수 수정
+	 * @param index
+	 * @param scores
+	 */
+	public void updateScores(int index, StudentDTO other) {
+
+		// 객체 배열 : 객체 참조형 변수를 묶음으로 다룸
+		
+		// students[index] 번째에 저장된 주소 얕은 복사
+		StudentDTO s = students[index];
+		
+		s.setHtml(other.getHtml());
+		s.setCss(other.getCss());
+		s.setJs(other.getJs());
+		s.setJava(other.getJava());
+	}
+
+
 	
-	
-	
+	/**
+	 * 평균 최대/최소 구하기
+	 * @return 
+	  	최고점 : 짱구(85.4)
+	  	최저점 : 맹구(61.5)
+	 */
+	public String selectMaxMin() {
+		
+		double maxAvg = 0.0;
+		double minAvg = 0.0;
+		
+		String maxName = null;
+		String minName = null;
+		
+		// 최고점/최저점 판별
+		for(StudentDTO std : students) {
+			
+			if(std == null) break; // 학생이 없으면 반복 멈춤
+			
+			int sum = std.getHtml() + std.getCss() + std.getJs() + std.getJava();
+			
+			double avg = sum / 4.0;
+			
+			// for 문 첫 반복일 때
+			if(maxAvg == 0.0) {
+				maxAvg = avg;
+				maxName = std.getName();
+
+				minAvg = avg;
+				minName = std.getName();
+				continue;
+			}
+			
+			if(avg > maxAvg) { // 기존 최댓값보다 현재 평균이 클 때
+				maxAvg = avg;
+				maxName = std.getName();
+				
+			}
+			if(avg < minAvg) { // 기존 최댓값보다 현재 평균이 작을 때
+				minAvg = avg;
+				minName = std.getName();
+				
+			}
+		}
+		
+		String result = String.format("최고점 : %s(%.1f)\n최저점 : %s(%.1f)",
+				maxName, maxAvg, minName, minAvg);
+		
+		return result;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Student복습/src/test/JaTe.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class JaTe {
+
+	public static void main(String[] args) {
+		
+		Scanner ab = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int i = ab.nextInt();
+		System.out.println("정수는 : " + i);
+	}
+	
+}
