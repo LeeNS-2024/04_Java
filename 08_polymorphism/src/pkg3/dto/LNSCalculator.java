67d34bf0f14a03fb4b83a60ea5f0cb5954346dee
LeeNS-2024 <dlsktj1@gmail.com>
2024-07-25 17:15:28 +0900

    20240725_수업 내용 커밋 ( 다형성Polymorphism )

---
+++ 08_polymorphism/src/pkg3/dto/LNSCalculator.java
@@ -0,0 +1,65 @@
+package pkg3.dto;
+
+// 인터페이스 상속 키워드 : implements (구현하다) 
+
+public class LNSCalculator implements Calculator{
+
+	@Override
+	public int plus(int a, int b) {
+		int result = a + b;
+		return 0;
+	}
+
+	@Override
+	public int minus(int a, int b) {
+		int result = a-b;
+		return result;
+	}
+
+	@Override
+	public int multi(int a, int b) {
+		int result = a * b;
+		return result;
+	}
+
+	@Override
+	public int div(int a, int b) {
+		int result = a / b;
+		return result;
+	}
+
+	@Override
+	public int mod(int a, int b) {
+		int result = a % b;
+		return result;
+	}
+
+	@Override
+	public int pow(int a, int x) {
+
+		 int result = 1;
+		 for(int i = 0; i<x; i++){
+			 result*= a;
+		 }
+		 return result;
+	}
+
+	@Override
+	public double areaOfCircle(double r) {
+		double result = PI*r*r;
+		return result;
+	}
+
+	@Override
+	public String toBinary(int num) {
+		return "이진수겠지 뭐,...";
+	}
+
+	@Override
+	public String toHexadecimal(int num) {
+		return "16진수 어쩌구겠지";
+	}
+	
+	
+
+}
