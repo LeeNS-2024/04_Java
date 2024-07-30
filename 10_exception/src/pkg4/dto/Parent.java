19b7b4a6c7c784588ebcf9d6aaeb1c43bcc65bcf
LeeNS-2024 <dlsktj1@gmail.com>
2024-07-30 17:43:00 +0900

    20240730_수업내용 커밋(오후) (Exception, Throws, Try - Catch, Finally)

---
+++ 10_exception/src/pkg4/dto/Parent.java
@@ -0,0 +1,14 @@
+package pkg4.dto;
+
+import java.io.IOException;
+
+public class Parent {
+	
+	
+	
+	public void method1() throws IOException{}
+	
+	public void method2() throws IOException{}
+	
+	public void method3() throws IOException{}
+}
