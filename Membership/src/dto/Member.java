f794105ab65dcb8058e690f6e96e8be1b186d7ef
LeeNS-2024 <dlsktj1@gmail.com>
2024-08-02 17:24:32 +0900

    20240802_수업내용커밋(Membership 3,4,5,6) (TodoList 만들기 틀잡음 ( 월요일까지 완성))

--- Membership/src/dto/Member.java
+++ Membership/src/dto/Member.java
@@ -2,6 +2,7 @@ package dto;
 
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.ToString;
 @NoArgsConstructor // 기본 생성자
 @AllArgsConstructor // 전체 필드 매개 변수 생성자
 @ToString // toString() 오버라이딩
+@EqualsAndHashCode // equals(), hashCode() 오버라이딩
 public class Member implements Serializable{ // + 직렬화
 	
 	// DTO(Data Transfer Object) : 데이터 전달용 객체
@@ -29,4 +31,6 @@ public class Member implements Serializable{ // + 직렬화
 	private String phone;
 	private int amount;
 	private int grade;
+	
+	
 }
